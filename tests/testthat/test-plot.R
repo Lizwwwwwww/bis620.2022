914e9cdef8647fdf15ae7ccccc93cb87a9f2d322
Lizwwwwwww <2282188178@qq.com>
2022-11-10 12:19:37 -0500

    updated something

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -19,7 +19,7 @@ test_that(
   "The accel_plot() is correct for time-series data.",
   {
     data(ukb_accel)
-    p = accel_plot(ukb_accel[1:100,]) + ggplot2::theme_minimal()
+    p = accel_plot(ukb_accel[1:100,])
     expect_doppelganger("first-100-samples", p) #p is ggolot project compare with
   }
 )
