290b9f6dbfcf6c40f2310c1136608449422bd699
Lizwwwwwww <2282188178@qq.com>
2022-11-11 22:44:23 -0500

    Changed test-plot.R to increase the test coverage. Added a github action to calculate the test coverage. Added a github action to lint the package.

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -23,3 +23,26 @@ test_that(
     expect_doppelganger("first-100-samples", p) #p is ggolot project compare with
   }
 )
+
+
+test_that(
+  "The spectral_signature() is correct for a data.frame with columns X, Y, Z, and time.",
+  {
+    data(ukb_accel)
+    p = ukb_accel[1:100,] |>
+      spectral_signature(take_log = FALSE) |>
+      accel_plot()
+    expect_doppelganger("spectral signature", p)
+  }
+)
+
+test_that(
+  "The spectral_signature() using the log of the modulus is correct for a data.frame with columns X, Y, Z, and time.",
+  {
+    data(ukb_accel)
+    p = ukb_accel[1:100,] |>
+      spectral_signature(take_log = TRUE) |>
+      accel_plot()
+    expect_doppelganger("spectral signature take log", p)
+  }
+)
